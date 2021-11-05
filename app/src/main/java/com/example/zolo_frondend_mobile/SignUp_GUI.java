24ad86f570db1672f9f601558419ab23442e77fb
18074161 <thaihuyhoang0610@gmail.com>
2021-11-05 16:51:24 +0700

    Giao diện đăng kí sửa

--- app/src/main/java/com/example/zolo_frondend_mobile/SignUp_GUI.java
+++ app/src/main/java/com/example/zolo_frondend_mobile/SignUp_GUI.java
@@ -3,12 +3,21 @@ package com.example.zolo_frondend_mobile;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SignUp_GUI extends AppCompatActivity {
-
+    EditText edtSDT, edtMk, edtTen, edtTuoi;
+    Button btnSignUp;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        edtSDT = findViewById(R.id.edtSDT);
+        edtMk = findViewById(R.id.edtMk);
+        edtTen = findViewById(R.id.edtTen);
+        edtTuoi = findViewById(R.id.edtTuoi);
+
+
     }
 }
